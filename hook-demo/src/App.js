71d64f7629c9d3caf0083b2f8735afe40a6f2c4d
chenle <chenle@forchange.tech>
2021-09-17 20:38:53 +0800

    feat: 修改例子

--- hook-demo/src/App.js
+++ hook-demo/src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import useXState from './hooks/useXState'
+import useTitle from './hooks/useTitle'
+import useUpdate from './hooks/useUpdate'
 
 function App() {
   const [number, setNumber] = useXState(0)
+  const update = useUpdate()
 
   const handleSetNumber = () => {
     setNumber(number + 1, (state) => {
       console.log(state, '=====')
     })
   }
+
+  useTitle('hooks-demo')
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +22,8 @@ function App() {
           {number}
         </p>
         <button onClick={handleSetNumber}/>
+        {Date.now()}
+        <button onClick={update}/>
       </header>
     </div>
   );
