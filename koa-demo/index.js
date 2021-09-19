5a1cca6b99a40baf09c9820b12b52c921589d344
chenle <chenle@forchange.tech>
2021-09-19 10:10:27 +0800

    feat: 添加中间价重复调用判断

--- koa-demo/index.js
+++ koa-demo/index.js
@@ -8,8 +8,14 @@ class Koa {
   }
   compose(middleware) {
     return (ctx, next) => {
+      let index = -1;
       return dispatch(0);
       function dispatch(i) {
+        //这里的i 是标识 我即将要去执行哪一个中间件  而 index 是标识 我上一次执行的是哪一个中间件
+        //当 i <= index 时意味着我即将要去执行我已经执行过的中间件，这是违背洋葱模型的本质的，这时将会报错为 next 函数被调用执行多次
+        if (i <= index)
+          return Promise.reject(new Error("next() called multiple times"));
+        index = i; // 更换辨识
         let fn = middleware[i];
         if (i === middleware.length) next = null;
         if (!fn) return Promise.resolve();
